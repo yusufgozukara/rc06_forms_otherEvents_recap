09ba3e5d55dc48444755f5edffb4908a1b2f7bf6
Yusuf Gözükara <101390306+yusufgozukara@users.noreply.github.com>
2023-06-18 18:48:17 +0300

    m4

--- src/components/2-keyboardClipboardEvents/KeyboardClipboard.js
+++ src/components/2-keyboardClipboardEvents/KeyboardClipboard.js
@@ -9,6 +9,20 @@ const KeyboardClipboard = () => {
     e.keyCode === 13 && alert("Onaylıyo musun");
   };
 
+  const handleAreaPaste = (e) => {
+    e.target.value += e.clipboardData.getData("text").toLowerCase();
+    e.target.style.border = "3px solid red";
+    e.target.style.backgroundColor = "lightgreen";
+    e.preventDefault();
+  };
+
+  const handleAreaChange = (e) => {
+    if (!e.target.value) {
+      e.target.style.border = "1px solid black";
+      e.target.style.backgroundColor = "white";
+    }
+  };
+
   return (
     <div className="container text-center">
       <h2>Clipboard event</h2>
@@ -19,7 +33,17 @@ const KeyboardClipboard = () => {
         onKeyDown={handleKeyDown}
       />
 
-      <p className="text-start m-4">{inputValue}</p>
+      <p onCopy={() => alert("Dikkat copy")} className="text-start m-4">
+        {inputValue}
+      </p>
+      <textarea
+        name="area"
+        id="area"
+        cols="50"
+        rows="10"
+        onPaste={handleAreaPaste}
+        onChange={handleAreaChange}
+      ></textarea>
     </div>
   );
 };
