d5c854c50b16db3c497e3f8f6eefa87b9a904526
Yusuf Gözükara <101390306+yusufgozukara@users.noreply.github.com>
2023-06-18 16:14:56 +0300

    form1

--- src/components/1-forms/Form.jsx
+++ src/components/1-forms/Form.jsx
@@ -1,26 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsername = (e) => {
+    console.log(e.target.value);
+    setUsername(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`username : ${username}
+    email : ${email}
+    password : ${password}
+    `);
+  };
+
   return (
     <div>
-      <form style={{ margin: "5rem" }}>
+      <form style={{ margin: "5rem" }} onSubmit={handleSubmit}>
         <div class="mb-3">
           <label htmlFor="username" class="form-label">
-            Username
+            Username : <span>{username}</span>
           </label>
-          <input type="text" className="form-control" id="username" />
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            value={username}
+            onChange={handleUsername}
+          />
         </div>
         <div class="mb-3">
           <label htmlFor="email" class="form-label">
-            Email address
+            Email address : <span>{email}</span>
           </label>
-          <input type="email" className="form-control" id="email" />
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div className="mb-3">
           <label htmlFor="password" className="form-label">
             Password
           </label>
-          <input type="password" className="form-control" id="password" />
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
 
         <button type="submit" className="btn btn-primary">
